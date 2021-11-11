e5bdb3eee10fbb4d247ff5705b9b5c7454e65383
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-11-11 21:07:48 +0530

    add font family and change color object

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-  purge: ['./public/**/*.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        'dark-main': '#18191A',
-        'dark-second': '#242526',
-        'dark-third': '#3A3B3C',
-        'dark-first': '#B8BBBF',
-      },
-    },
-  },
-  variants: {
-    extend: {
-      display: ['group-hover'],
-      transform: ['group-hover'],
-      scale: ['group-hover'],
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+	purge: ["./public/**/*.html"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				"dark-main": "#18191A",
+				"dark-second": "#242526",
+				"dark-third": "#3A3B3C",
+				"dark-text": "#B8BBBF",
+			},
+			fontFamily: {
+				sans: ["Netflix Sans Regular"],
+			},
+		},
+	},
+	variants: {
+		extend: {
+			display: ["group-hover"],
+			transform: ["group-hover"],
+			scale: ["group-hover"],
+		},
+	},
+	plugins: [require("@tailwindcss/forms")],
+};
